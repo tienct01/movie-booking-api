855e2bda0222d571cbbaad925b18dfdb3c1cf495
SBC-TienDVD <tiendvd@bsscommerce.com>
2023-10-09 23:17:12 +0700

    add vnpay

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,6 +5,7 @@ const empRoute = require('./emp.route');
 const scheduleRoute = require('./schedule.route');
 const authRoute = require('./auth.route');
 const authMiddleware = require('../middlewares/auth.middleware');
+const vnPayRouter = require('./vnpay.route.js');
 
 const initRoute = app => {
   app.use('/movies', movieRoute);
@@ -13,6 +14,7 @@ const initRoute = app => {
   app.use('/employees', authMiddleware.checkAdmin, empRoute);
   app.use('/schedules',authMiddleware.checkLogin, scheduleRoute);
   app.use('/auth', authRoute);
+  app.use("/checkout", vnPayRouter);
   app.use((data, req, res, next) => {
     console.log('Handling error middleware', data);
     res.status(500).json({
